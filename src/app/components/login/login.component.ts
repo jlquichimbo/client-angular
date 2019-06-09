999c6bdab1a184e7ae319be2c9815bc9d59e0961
Jose Luis <joseluisquichimbo91@gmail.com>
2019-06-09 09:39:44 -0500

    Corregido error pattern login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,39 @@
+import { User } from './../../models/user';
+import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent implements OnInit {
+  public titulo: string;
+  public usuario: User;
+  public estado: string;
+  public identidad;
+  public token;
+
+  constructor(private route: ActivatedRoute, private router: Router, private usuarioService: UserService) {
+    this.titulo = 'Iniciar sesion';
+    this.usuario = new User();
+  }
 
   ngOnInit() {
+    console.log('Componente iniciar-sesion cargado');
+  }
+
+  iniciarSesion(form: NgForm) {
+    this.usuarioService.iniciarSesion(this.usuario).subscribe(res => console.log(res), error => console.log(error as any));
+  }
+
+  obtenerToken() {
+    this.usuarioService.iniciarSesion(this.usuario, 'true').subscribe(res => console.log(res), error => console.log(error as any));
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,59 @@
+import { Global } from './global';
+import { User } from './../models/user';
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  public url: string;
+  public usuarioSeleccionado: User;
+  public usuarios: User[];
+  public identidad;
+  public token;
 
-  constructor() { }
+  constructor(public http: HttpClient) {
+    this.usuarioSeleccionado = new User();
+    this.url = Global.url;
+  }
+
+  // Login
+  iniciarSesion(usuario, token = null): Observable<any> {
+    if (token != null) {
+      usuario.token = token;
+    }
+
+    const params = JSON.stringify(usuario);
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this.http.post(this.url + 'login', params, {headers});
+  }
+
+  // Que usuario se ha logueado
+  obtenerIdentidad() {
+    const identidad = JSON.parse(localStorage.getItem('identidad'));
+
+    if (identidad !== 'undefined') {
+      this.identidad = identidad;
+    } else {
+      this.identidad = null;
+    }
+
+    return this.identidad;
+  }
+
+  obtenerToken() {
+    // Guardando variable en memoria de navegador
+    const token = localStorage.getItem('token');
+
+    if (token !== 'undefined') {
+      this.token = token;
+    } else {
+      this.token = null;
+    }
+
+    return this.token;
+  }
 }
